ace148a2c41a9ee05c036bdfcea818b4479ffbea
Eugen Bejenaru <48156218+EugenBj@users.noreply.github.com>
2020-08-05 08:11:03 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+// 'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
+
+// sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+// gulp.task('css', function () {
+//   gulp.src('./css/*.css')
+//     .pipe(uglifycss({
+//       "uglyComments": true
+//     }))
+//     .pipe(gulp.dest('./dist/'));
+// });
+
+gulp.task('run', ['sass']);
+
+gulp.task('watch', function(){
+  gulp.watch('./sass/*.sass', ['sass']);
+  // gulp.watch('./css/*.css', ['css']);
+});
+
+gulp.task('default', ['run', 'watch']);
